9161acbac504ab2c4669425926c6b2976e193828
John <smythj6@gmail.com>
2024-02-26 11:44:05 +0000

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -10,11 +10,11 @@ percentages = document.querySelectorAll("div.row.styles_depositCardMain___3a-Kb 
 amounts = document.querySelectorAll("styles_currentDepositAmount___1bGpu");
 maturities = document.querySelectorAll("div.row.styles_depositCardMain___3a-Kb > div.styles_progressBar___2blqr > div > div > div > span");
 
-infos = percentages.map(function(val, index) {
+infos = percentages.foreach(function(val, index) {
   new Info(amounts[index], val, maturities[index]);
 })
 
-document.querySelectorAll("div.row.styles_depositCardMain___3a-Kb > div.styles_progressBar___2blqr > div").map(function (div, index) {
+document.querySelectorAll("div.row.styles_depositCardMain___3a-Kb > div.styles_progressBar___2blqr > div").foreach(function (div, index) {
   span = document.createElement("span");
   span.innerText = "€" + infos[index].percentage / 100 * infos[index].amount * infos[index].maturity;
   span.style.display = "block";
